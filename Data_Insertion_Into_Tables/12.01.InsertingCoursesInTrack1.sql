d6f9a6e9464874f1002621bb5d778eb2b7ad951e
MarizSedhom <marizsedhom@gmail.com>
2025-02-01 20:10:36 +0200

    adding script to Insert Courses In Track 1

---
+++ Data_Insertion_Into_Tables/12.01.InsertingCoursesInTrack1.sql
@@ -0,0 +1,18 @@
+EXEC InsertCourseInTrack @TrackID = 1, @CourseID = 1;
+GO
+EXEC InsertCourseInTrack @TrackID = 1, @CourseID = 2;
+GO
+EXEC InsertCourseInTrack @TrackID = 1, @CourseID = 3;
+GO
+EXEC InsertCourseInTrack @TrackID = 1, @CourseID = 4;
+GO
+EXEC InsertCourseInTrack @TrackID = 1, @CourseID = 5;
+GO
+EXEC InsertCourseInTrack @TrackID = 1, @CourseID = 6;
+GO
+EXEC InsertCourseInTrack @TrackID = 1, @CourseID = 12;
+GO
+EXEC InsertCourseInTrack @TrackID = 1, @CourseID = 13;
+GO
+EXEC InsertCourseInTrack @TrackID = 1, @CourseID = 14;
+GO
